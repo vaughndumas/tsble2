84159df0610daf2745f4d11588a1a16d5f41f5fb
vaughndumas <vaughndumas@192.168.1.25>
2015-01-13 16:17:15 +1100

    Daily checking.  Busy trying to get the POST to dev1 working.

--- platforms/ios/www/app.js
+++ platforms/ios/www/app.js
@@ -1,5 +1,48 @@
 // JavaScript code for the BLE Scan example app.
-
+function send_dev1(x_device) {
+    v_date = new Date();
+    v_dts = v_date.getFullYear() + "-" +
+                                v_date.getMonth()+1 + "-" +
+                                v_date.getDate() + " " +
+                                v_date.getHours() + ":" +
+                                v_date.getMinutes() + ":" +
+                                v_date.getSeconds();
+    v_xml = '<?xml version="1.0" encoding="UTF-8"?>'
+          + '<document_root>'
+          + '<custcode>TS</custcode>'
+          + '<hardwareType>1</hardwareType>'
+          + '<datetimesaved>' + v_dts + '</datetimesaved>'
+          + '<gmtdatetime>' + v_dts + '</gmtdatetime>'
+          + '<hardwareId>' + '455096622' + '</hardwareId>'
+          + '<latitude>' + vg_position.coords.latitude + '</latitude>'
+          + '<longitude>' + vg_position.coords.longitude + '</longitude>'
+//          + '<heading>' + vg_position.coords.heading + '</heading>'
+//          + '<speed>' + vg_position.coords.speed + '</speed>'
+          + '<altitude>' + vg_position.coords.altitude + '</altitude>'
+          + '<eventtype>1001</eventtype>'
+          + '<eventcode>0</eventcode>'
+          + '<messageID>100</messageID>'
+          + '<RSSI>' + x_device.rssi + '</RSSI>'
+          + '<DriverID>' + x_device.name + '</DriverID>'
+          + '</document_root>';
+    var v_xml_doc = $.parseXML(v_xml);
+    alert(v_xml);
+    $(function() {
+        $.ajax({
+            url:  "http://dev1.thing-server.com/Thingevents",
+            type: "POST",
+            processData: false,
+            data: {xml: v_xml_doc}
+        })
+        .done(function(x_data) {
+            v_return = x_data;
+            alert(v_return);
+        })
+        .error(function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText);
+        })
+    });
+}
 // Application object.
 var app = {};
 
@@ -62,7 +105,6 @@ app.stopScan = function()
 // Called when Start Scan button is selected.
 app.ui.onStartScanButton = function()
 {
-    alert("in OnStartScanButton");
 	app.startScan(app.ui.deviceFound);
 	app.ui.displayStatus('Scanning...');
 	app.ui.updateTimer = setInterval(app.ui.displayDeviceList, 500);
@@ -115,6 +157,10 @@ app.ui.displayDeviceList = function()
 			else if (device.rssi < 0) { rssiWidth = 100 + device.rssi; }
 
 			// Create tag for device data.
+                        if (device.advertisementData.kCBAdvDataIsConnectable)
+                            v_contact = 'Is connectable';
+                        else
+                            v_contact = 'Is NOT connectable';
 			var element = $(
 				'<li>'
 				+	'<strong>' + device.name + '</strong><br />'
@@ -124,10 +170,12 @@ app.ui.displayDeviceList = function()
 				+	device.rssi + '<br />'
 				+ 	'<div style="background:rgb(225,0,0);height:20px;width:'
 				+ 		rssiWidth + '%;"></div>'
+                                +       '<br />' + v_contact                        
 				+ '</li>'
 			);
 
 			$('#found-devices').append(element);
+                        send_dev1(device);
 		}
 	});
 };
